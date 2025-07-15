3eede3c4396b91f193aa7236e51b166d0f55312b
Robin Hagmayer <robin.hagmayer@proton.me>
2025-07-15 17:46:45 +0200

    feat: added login functionality

--- main.go
+++ main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/RobinHagmayer/Gator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
 func main() {
-	cfg := config.Read()
-	cfg.SetUser("robin")
-	cfgNew := config.Read()
-	fmt.Printf("%+v\n", cfgNew)
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	programState := &state{cfg: &cfg}
+	cmds := commands{registeredCommands: make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Error: to few arguments")
+		os.Exit(1)
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+	cmd := command{
+		name: cmdName,
+		args: cmdArgs,
+	}
+
+	err = cmds.run(programState, cmd)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
