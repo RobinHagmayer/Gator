f7a300ea44cabb4594dee3c8cab44184d5828b79
Robin Hagmayer <robin.hagmayer@proton.me>
2025-07-21 15:23:10 +0200

    feat: added users command to list all users

--- handlers.go
+++ handlers.go
@@ -64,3 +64,20 @@ func handlerReset(s *state, cmd command) error {
 	fmt.Println("Deleted all users!")
 	return nil
 }
+
+func handlerUser(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("Couldn't fetch all users: %w", err)
+	}
+
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %s (current)\n", user.Name)
+			continue
+		}
+		fmt.Printf("* %s\n", user.Name)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerUser)
 
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "Error: to few arguments")
